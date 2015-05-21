84bfdd1ed0641d206c44c1e33a6c4d03e7cfc61d
Ian Juma <ian.juma@yahoo.com>
2015-05-21 17:18:16 +0300

    ussd

--- app/api/resource.py
+++ app/api/resource.py
@@ -6,11 +6,15 @@ from flask import (make_response, abort, request)
 from json import dumps
 from _utils import (FetchUrl, MakeRequests)
 
+# globals
+from app import r
+from app import g
+from app import RqlError
+
 
 @app.route('/api/ussd', methods=['POST'])
-def receiveUSSD():
+def receive_ussd():
     """
-    :param USSD object
     :return: get menu and all - USSD
     """
     if request.method != 'POST':
@@ -59,3 +63,46 @@ def get_shortcode():
     resp.headers['Content-Type'] = 'application/json'
     resp.cache_control.no_cache = True
     return resp
+
+
+@app.route('/api/voice/callback/', methods=['POST'])
+def voice_callback():
+    if request.method is 'POST':
+        if request.headers['Content-Type'] != 'text/plain':
+            abort(400)
+
+        # Reads the variables sent via POST from our gateway
+        session_id = request.args.get("sessionId")
+        service_code = request.args.get("serviceCode")
+        phone_number = request.args.get("phoneNumber")
+        text = request.args.get("text")
+
+        if request.args.get('text') is '':
+            # load menu
+            menu_text = """CON Africa's-Talking Show and Tell Demo \n
+            - You're  registered we'll call you and ask you a few questions\n
+            - You stand a chance to win airtime \n
+            END
+            """
+
+            resp = make_response(menu_text, 200)
+            resp.headers['Content-Type'] = "text/plain"
+            resp.cache_control.no_cache = True
+            return resp
+
+        try:
+            tasks = r.table('User').insert({'phoneNumber': phone_number, 'serviceCode': service_code,
+                                            'sessionId': session_id, 'text': text}).run(g.rdb_conn)
+            # push to queue
+        except RqlError:
+            logging.warning('DB code verify failed on /api/ussd/ - > callback')
+
+            resp = make_response(jsonify({"Error": "503 DB error"}), 503)
+            resp.headers['Content-Type'] = "application/json"
+            resp.cache_control.no_cache = True
+            return resp
+
+        resp = make_response(tasks, 200)
+        resp.headers['Content-Type'] = "application/json"
+        resp.cache_control.no_cache = True
+        return resp
--- app/api/ussd.py
+++ app/api/ussd.py
@@ -3,7 +3,7 @@ from flask import (abort, request, make_response, jsonify)
 from _utils import (FetchUrl, MakeRequests, WriteBulkSmsCsv)
 
 from app import r
-from app import redis
+from app import g
 from app import RqlError
 
 
@@ -77,31 +77,24 @@ def get_tasks():
         return resp
 
 
-def receivePayment():
-    pass
-
-
 @app.route('/api/getUSSD/', methods=['POST'])
-def ussdCallBack():
+def ussd_callback():
     if request.method is 'POST':
         if request.headers['Content-Type'] != 'text/plain':
             abort(400)
 
-        text = request.data
-
         # Reads the variables sent via POST from our gateway
-        sessionId   = request.args.get("sessionId")
-        serviceCode = request.args.get("serviceCode")
-        phoneNumber = request.args.get("phoneNumber")
-        text        = request.args.get("text")
+        session_id = request.args.get("sessionId")
+        service_code = request.args.get("serviceCode")
+        phone_number = request.args.get("phoneNumber")
+        text = request.args.get("text")
 
         if request.args.get('text') is '':
             # load menu
-            menu_text = """CON What would you like to do? \n
-            1. To pay a distributor \n
-            2. To check balance \n
-            3. To make a credit request \n
-            4. Check my transaction history \n
+            menu_text = """CON Africa's-Talking Show and Tell Demo \n
+            - You're  registered we'll call you and ask you a few questions\n
+            - You stand a chance to win airtime \n
+            END
             """
 
             resp = make_response(menu_text, 200)
@@ -109,41 +102,12 @@ def ussdCallBack():
             resp.cache_control.no_cache = True
             return resp
 
-        elif request.args.get('text') is '1':
-            # pay a distributor
-            balance = "END your balance is 2000 Kshs"
-
-            resp = make_response(balance, 200)
-            resp.headers['Content-Type'] = "text/plain"
-            resp.cache_control.no_cache = True
-            return resp
-
-        elif request.args.get('text') is '2':
-            balance = "END your balance is 2000 Kshs"
-            resp = make_response(balance, 200)
-
-            resp.headers['Content-Type'] = "text/plain"
-            resp.cache_control.no_cache = True
-            return resp
-
-        elif request.args.get('text') is '2':
-            balance = "END your balance is 2000 Kshs"
-            resp = make_response(balance, 200)
-
-            resp.headers['Content-Type'] = "text/plain"
-            resp.cache_control.no_cache = True
-            return resp
-        else:
-            balance = "END your balance is 2000 Kshs"
-            resp = make_response(balance, 200)
-            resp.headers['Content-Type'] = "text/plain"
-            resp.cache_control.no_cache = True
-            return resp
-
         try:
-            tasks = r.table('Client').get(sender).update(text).run(g.rdb_conn)
+            tasks = r.table('User').insert({'phoneNumber': phone_number, 'serviceCode': service_code,
+                                            'sessionId': session_id, 'text': text}).run(g.rdb_conn)
+            # push to queue
         except RqlError:
-            logging.warning('DB code verify failed on /api/getTasks/')
+            logging.warning('DB code verify failed on /api/ussd/ - > callback')
 
             resp = make_response(jsonify({"Error": "503 DB error"}), 503)
             resp.headers['Content-Type'] = "application/json"
@@ -156,19 +120,18 @@ def ussdCallBack():
         return resp
 
 
-def displayMenu():
-    menu_text = """CON What would you like to do? \n
-            1. To pay a distributor \n
-            2. To check balance \n
-            3. To make a credit request \n
-            4. Check my transaction history \n
+def display_menu():
+    menu_text = """CON Africa's-Talking Show and Tell Demo \n
+            1. You're  registered we'll call you and ask you a few questions\n
+            2. You stand a chance to win airtime \n
+            END
             """
     return menu_text
 
 
 def ussd_proceed(ussd_text):
-    return "CON %s" %(ussd_text)
+    return "CON %s" % ussd_text
 
 
 def ussd_stop(ussd_text):
-    return "END %s" %(ussd_text)
+    return "END %s" % ussd_text
