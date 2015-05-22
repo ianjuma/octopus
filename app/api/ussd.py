ba0c349e3d10f27af33641a7c857a115749ffad0
Ian Juma <ian.juma@yahoo.com>
2015-05-22 08:57:47 +0300

    all

--- app/api/ussd.py
+++ app/api/ussd.py
@@ -12,8 +12,9 @@ from _utils import consume_call
 @app.route('/api/ussd/callback/', methods=['POST'])
 def ussd_callback():
     if request.method == 'POST':
-        if request.headers['Content-Type'] != 'text/plain':
-            abort(400)
+        print request.headers['Content-Type']
+        #if request.headers['Content-Type'] != 'text/plain':
+        #    abort(400)
 
         # Reads the variables sent via POST from our gateway
         session_id = request.values.get("sessionId")
